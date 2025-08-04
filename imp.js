6b27158f98287135257b5b4b602420302d42c9cf
Diu-2001 <diudeep8@gmail.com>
2025-08-04 17:33:14 +0530

    Add files via upload

---
+++ imp.js
@@ -0,0 +1,2 @@
+document.write("<h1>JavaScript Implementation</h1>");
+document.write("<p>This is a simple JavaScript implementation.</p>");
---
+++ main.js
@@ -0,0 +1,22 @@
+let element;
+
+element = document.getElementById('header').setAttribute = ('class', 'xyz');
+
+console.log(element);
+
+let elements;
+
+elements = document.getElementsByClassName('list');
+
+console.log(elements);
+
+
+elements = document.getElementsByTagName('li');
+
+console.log(elements);
+
+elements = document.querySelectorAll('ul')[0].innerHTML;
+
+console.log(elements);
+
+
